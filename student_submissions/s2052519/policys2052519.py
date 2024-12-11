7f07b2c1153e6ae567be779243ebd1f50d0e5f5e
Khang Nguyễn <nguyendinhkhang0711@gmail.com>
2024-12-11 17:16:35 +0700

    :hammer: Implemented Column Generation Policy

--- student_submissions/s2052519/policys2052519.py
+++ student_submissions/s2052519/policys2052519.py
@@ -37,13 +37,12 @@ class ColumnGenerationPolicy:
     def __init__(self):
         self.patterns = []
         self.demands = None
-        self.stock = None
+        self.stocks = None
 
     def initialize(self, observation):
         self.demands = observation['products']
-        self.stock = observation['stocks']
+        self.stocks = observation['stocks']
 
-        self.patterns = []
         for product in self.demands:
             self.patterns.append({
                 "stock_idx": 0,
@@ -54,19 +53,9 @@ class ColumnGenerationPolicy:
     def solve_master_problem(self):
         c = np.ones(len(self.patterns))
 
-        A_eq = np.array(self.patterns).T
-        b_eq = np.array([product['quantity'] for product in self.demands])
-
-        res = linprog(c, A_eq=A_eq, b_eq=b_eq, bounds=(0, None), method="highs")
-
-        return res.x, res.fun
-
-    def solve_pricing_problem(self, duals):
-        c = np.ones(len(self.patterns))
-
         A_eq = []
         b_eq = [product["quantity"] for product in self.demands]
-        for product_idx, product in enumerate(self.demands):
+        for product in self.demands:
             row = []
             for pattern in self.patterns:
                 if np.array_equal(pattern["size"], product["size"]):
@@ -88,18 +77,25 @@ class ColumnGenerationPolicy:
             for product_idx, product in enumerate(self.demands):
                 product_height, product_width = product["size"]
 
-                for i in range(stock_height - product_height + 1):
-                    for j in range(stock_width - product_width + 1):
-                        subgrid = stock[i:i + product_height, j:j + product_width]
-                        if np.all(subgrid == -1):
-                            value = duals[product_idx]
-                            if value > max_value:
-                                max_value = value
-                                new_pattern = {
-                                    "stock_idx": stock_idx,
-                                    "size": product["size"],
-                                    "positions": [(i, j)]
-                                }
+                valid_positions = (stock == -1)
+                for i in range(product_height):
+                    valid_positions &= np.roll(stock == -1, shift=-i, axis=0)
+
+                for j in range(product_width):
+                    valid_positions &= np.roll(stock == -1, shift=-j, axis=1)
+
+                valid_indices = np.argwhere(valid_positions[:stock_height - product_height + 1, :stock_width - product_width + 1])
+
+                if len(valid_indices) > 0:
+                    for pos in valid_indices:
+                        value = duals[product_idx]
+                        if value > max_value:
+                            max_value = value
+                            new_pattern = {
+                                "stock_idx": stock_idx,
+                                "size": product["size"],
+                                "positions": [tuple(pos)]
+                            }
 
         return new_pattern
 
@@ -120,6 +116,8 @@ class ColumnGenerationPolicy:
                 "position": new_pattern["positions"][0]
             }
         else:
+            print("No new pattern found, stopping the iteration")
+
             return None
     
 
@@ -132,14 +130,14 @@ class Policy2052519(Policy):
         if policy_id == 1:
             self.policy = FirstCutPolicy()
         elif policy_id == 2:
-            pass
+            self.policy = ColumnGenerationPolicy()
 
     def get_action(self, observation, info):
         # Student code here
         if self.policy is not None:
             return self.policy.get_action(observation, info)
         else:
-            raise NotImplementedError("Policy ID 2 is not implemented yet.")
+            raise NotImplementedError("Policy is not implemented yet.")
 
     # Student code here
     # You can add more functions if needed
